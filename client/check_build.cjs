b02bee0af59a24288efd6c37fd8e5dcee1974a10
[iharshkumar] <srivastavaharsh1108@gmail.com>
2026-05-11 08:13:04 +0530

    Deleted unecessary files

--- client/check_build.cjs
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process');
-try {
-  console.log("Running npm run build...");
-  execSync('npm run build', { stdio: 'pipe' });
-  console.log("Build succeeded!");
-} catch (error) {
-  console.error("Build failed. First 1000 characters of stderr:");
-  console.error(error.stderr.toString('utf8').substring(0, 1000));
-}
