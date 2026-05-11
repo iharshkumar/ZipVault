946cf054482c92cc530a9882479dc61af71caf02
[iharshkumar] <srivastavaharsh1108@gmail.com>
2026-05-11 11:52:06 +0530

    Render deployment

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use('/api/upload', require('./routes/uploadRoutes'));
 // Serve static assets if in production (optional, for deployment readiness)
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
-    app.get('*', (req, res) =>
+    app.get('/(.*)', (req, res) =>
         res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'))
     );
 }
